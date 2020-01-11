e9359f57da55b1e9de30ac165de62c9131518405
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2020-01-11 20:53:06 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
   get '/articles/new' do
   erb :new
   end
- 
+
   post '/articles' do
    @article = Article.create(:title => params[:title], :content => params[:content])
    redirect to "/articles/#{@article.id}"
