b62e22a69bbc13e1c560a8446f9508b854993077
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2020-01-11 20:18:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,10 @@ class ApplicationController < Sinatra::Base
    erb :show
   end
 
+  delete '/articles/:id' do #delete action
+   @article = Article.find_by_id(params[:id])
+   @article.delete
+   redirect to '/articles'
+  end
+
 end
