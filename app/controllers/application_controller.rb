eb230f5d32049c953dc325d2a6c430c28eb54ffb
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2020-01-11 20:42:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,12 @@ class ApplicationController < Sinatra::Base
     @recipee = Recipe.find_by_id(params[:id])
     erb :edit
   end
+
+  patch '/articles/:id' do #edit action
+  @article = Article.find_by_id(params[:id])
+  @article.title = params[:title]
+  @article.content = params[:content]
+  @article.save
+  redirect to "/articles/#{@article.id}"
+end
 end
